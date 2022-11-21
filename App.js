097f7872dd0f14d9f6306ea00bba67013f113a47
Shilpa M L <shilpaml@ShilpaML-iMac.local>
2022-11-21 09:27:30 +0530

    commit

--- App.js
+++ App.js
@@ -15,12 +15,8 @@ import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistStore} from 'redux-persist';
 import {Router} from './src/navigation/Route';
-<<<<<<< HEAD
 import {ForgotPassword} from './src/screens/ForgotPassword';
 import { Test } from './src/screens/Test';
-=======
-
->>>>>>> a01174a03262e6556c96ee1e0c5631187fa3333d
 
 let persistor = persistStore(store);
 
@@ -28,11 +24,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-<<<<<<< HEAD
         <Test/>
-=======
-        <Router/>
->>>>>>> a01174a03262e6556c96ee1e0c5631187fa3333d
       </PersistGate>
     </Provider>
   
