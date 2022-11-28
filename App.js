01d217ec887cfdcc79549f39fb48fb571508ea4f
Shilpa M L <shilpaml@ShilpaML-iMac.local>
2022-11-28 11:41:03 +0530

    certificate page

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <TestStack />
+        <Router />
       </PersistGate>
     </Provider>
   );
